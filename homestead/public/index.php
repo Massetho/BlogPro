e197644b6f944829da2d9565c748a3cc90afad24
Massetho <quentin_thomasset@hotmail.fr>
2018-01-17 15:50:48 +0100

    #2 - Pull-Request Corrections

--- homestead/public/index.php
+++ homestead/public/index.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: quent
+ * @description : first file called by the client
+ * @author: Quentin Thomasset
+ * @package: BlogPro
  * Date: 02/10/2017
  * Time: 14:50
  */
@@ -9,10 +10,11 @@
 namespace App;
 use App\Model\Application;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../App/Config/config.php';
+
+error_reporting(_ERROR_REPORTING_);
+ini_set('display_errors', 1);
 
 $app = new Application;
 $app->run();
