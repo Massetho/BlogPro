8ec1d2f556dd9fb28ebe69b9892400b2b595f1da
Massetho <quentin_thomasset@hotmail.fr>
2018-01-29 17:51:26 +0100

    #12 - Category entity/manager added

---
+++ App/Model/Entity/Category.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @description : Article Category table
+ * @Author : Quentin Thomasset
+ * @package : BlogPro
+ * Date: 19/01/2018
+ * Time: 16:08
+ */
+namespace App\Model\Entity;
+
+class Category extends AbstractEntity
+{
+
+}
---
+++ App/Model/Manager/CategoryManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @description : Article Category table
+ * @Author : Quentin Thomasset
+ * @package : BlogPro
+ * Date: 19/01/2018
+ * Time: 16:10
+ */
+namespace App\Model\Manager;
+
+use App\Model\Database;
+use App\Model\Entity\AbstractEntity;
+use App\Model\Entity\Article;
+
+
+class CategoryManager extends AbstractManager
+{
+
+}
